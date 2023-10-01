55f1b817aab35495acf17cd051e30c3f113103a3
PenguinHerder <skutelaandrzej@yahoo.com>
2023-10-01 23:42:08 +0100

    amla wip

--- lua/amla.lua
+++ lua/amla.lua
@@ -3,41 +3,72 @@ local find_amla_buffs = function(amla_settings)
 
 	if amla_settings['has_default_regenerates'] then
 		allowed_amlas[#allowed_amlas + 1] = 'REGEN'
+	elseif amla_settings['is_healthy'] then
+		allowed_amlas[#allowed_amlas + 1] = 'MINI_REG'
+	end
+	
+	if amla_settings['has_ranged'] then
+		allowed_amlas[#allowed_amlas + 1] = 'R_DMG'
+
+		if amla_settings['is_dextrous'] then
+			allowed_amlas[#allowed_amlas + 1] = 'R_STR'
+		end
+		
+		if amla_settings['has_magical_ranged'] then
+			if amla_settings['is_intelligent'] then
+				allowed_amlas[#allowed_amlas + 1] = 'R_MAG_ACC'
+			end
+		else
+			allowed_amlas[#allowed_amlas + 1] = 'R_ACC'
+
+			if amla_settings['is_quick'] or amla_settings['is_undead'] then
+				allowed_amlas[#allowed_amlas + 1] = 'R_PAR'
+			end
+		end
+	end
+	
+	if amla_settings['has_melee'] then
+		allowed_amlas[#allowed_amlas + 1] = 'M_DMG'
+
+		if amla_settings['is_resilient'] or amla_settings['is_undead'] then
+			allowed_amlas[#allowed_amlas + 1] = 'M_STR'
+		end
+		
+		if amla_settings['has_magical_melee'] then
+			if amla_settings['is_strong'] then
+				allowed_amlas[#allowed_amlas + 1] = 'M_MAG_ACC'
+			end
+		else
+			allowed_amlas[#allowed_amlas + 1] = 'M_ACC'
+
+			if amla_settings['is_quick'] then
+				allowed_amlas[#allowed_amlas + 1] = 'M_PAR'
+			end
+		end
+	end
+	
+	if amla_settings['is_fast'] and amla_settings['is_quick'] then
+		allowed_amlas[#allowed_amlas + 1] = 'SPEED'
+	elseif amla_settings['is_fast'] or amla_settings['is_quick'] then
+		allowed_amlas[#allowed_amlas + 1] = 'SMALL_SPEED'
 	end
 
+	if amla_settings['is_intelligent'] then
+		allowed_amlas[#allowed_amlas + 1] = 'LOW_XP'
+	end
 
+	if amla_settings['is_resilient'] or amla_settings['is_mechanical'] then
+		allowed_amlas[#allowed_amlas + 1] = 'PHYS_RES'
+	end
 
-	
-	-- if amla_settings['has_ranged'] then
-	-- 	for k,v in ipairs({ 'A28', }) do allowed_amlas[#allowed_amlas + 1] = v end
-		
-	-- 	if amla_settings['has_magical_ranged'] then
-	-- 		for k,v in ipairs({ 'A42', }) do allowed_amlas[#allowed_amlas + 1] = v end
-	-- 	else
-	-- 		for k,v in ipairs({ 'B13', }) do allowed_amlas[#allowed_amlas + 1] = v end
-	-- 	end
-	-- elseif amla_settings['has_melee'] then
-	-- 	for k,v in ipairs({ 'A4', }) do allowed_amlas[#allowed_amlas + 1] = v end
-	-- end
-	
-	-- if amla_settings['has_melee'] then
-	-- 	for k,v in ipairs({ 'A2', }) do allowed_amlas[#allowed_amlas + 1] = v end
-		
-	-- 	if amla_settings['has_magical_melee'] then
-	-- 		for k,v in ipairs({ 'A45', }) do allowed_amlas[#allowed_amlas + 1] = v end
-	-- 	else
-	-- 		for k,v in ipairs({ 'A9', }) do allowed_amlas[#allowed_amlas + 1] = v end
-	-- 	end
-	-- end
-	
-	-- if amla_settings['is_fast'] then
-	-- 	for k,v in ipairs({ 'B5' }) do allowed_amlas[#allowed_amlas + 1] = v end
-	-- end
-	
-	-- if amla_settings['has_physical_resistances'] then
-	-- 	for k,v in ipairs({ 'A25'  }) do allowed_amlas[#allowed_amlas + 1] = v end
-	-- end
+	if amla_settings['is_undead'] then
+		allowed_amlas[#allowed_amlas + 1] = 'ARCANE_RES'
+	end
 
+	if amla_settings['is_elemental'] then
+		allowed_amlas[#allowed_amlas + 1] = 'ELEM_RES'
+	end
+	
 	return allowed_amlas
 end
 
@@ -47,13 +78,20 @@ function wesnoth.wml_actions.qquws_generate_random_amla_list(cfg)
 	local amla_settings = {
 		['level'] = wml.variables['qquws_amla_data.level'],
 		['has_default_regenerates'] = wml.variables['qquws_amla_data.has_default_regenerates'],
-
 		['has_ranged'] = wml.variables["qquws_amla_data.has_ranged_attack"],
 		['has_melee'] = wml.variables["qquws_amla_data.has_melee_attack"],
 		['has_magical_ranged'] = wml.variables["qquws_amla_data.has_magical_ranged"],
 		['has_magical_melee'] = wml.variables["qquws_amla_data.has_magical_melee"],
 		['is_fast'] = wml.variables["qquws_amla_data.is_fast"],
-		['has_physical_resistances'] = wml.variables["qquws_amla_data.has_physical_resistances"],
+		['is_strong'] = wml.variables["qquws_amla_data.is_strong"],
+		['is_dextrous'] = wml.variables["qquws_amla_data.is_dextrous"],
+		['is_quick'] = wml.variables["qquws_amla_data.is_quick"],
+		['is_resilient'] = wml.variables["qquws_amla_data.is_resilient"],
+		['is_intelligent'] = wml.variables["qquws_amla_data.is_intelligent"],
+		['is_healthy'] = wml.variables["qquws_amla_data.is_healthy"],
+		['is_undead'] = wml.variables["qquws_amla_data.is_undead"],
+		['is_mechanical'] = wml.variables["qquws_amla_data.is_mechanical"],
+		['is_elemental'] = wml.variables["qquws_amla_data.is_elemental"],
 	}
 
 	local allowed_amlas = find_amla_buffs(amla_settings)
