68b003345c8f3bd8229daedea74c97573edc2c6c
PenguinHerder <skutelaandrzej@yahoo.com>
2024-07-08 13:57:28 +0100

    various

--- lua/rules_4.lua
+++ lua/rules_4.lua
@@ -545,20 +545,28 @@ return {
 	
 	-- 156 - 162 (mask y 14 - 8)
 	
-	{ ['t'] = 157, ['d'] = 1, ['side'] = 4, ['x'] = 25, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['buff'] = 'A41:B9:C19', ['race_guard_off'] = true, ['allow_random'] = false },
-	{ ['t'] = 158, ['d'] = 1, ['side'] = 4, ['x'] = 24, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['buff'] = 'A20:B41:C9', ['race_guard_off'] = true, ['allow_random'] = false },
-	{ ['t'] = 158, ['d'] = 1, ['side'] = 4, ['x'] = 23, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['buff'] = 'A30:B41:C13', ['race_guard_off'] = true, ['allow_random'] = false },
-	{ ['t'] = 158, ['d'] = 1, ['side'] = 4, ['x'] = 25, ['lvl'] = 6, ['type'] = 'QQ_adjudicator', ['default'] = 'QQ_adjudicator', ['buff'] = 'A23:B7:C29', ['guard'] = true },
+	{ ['t'] = 157, ['d'] = 1, ['side'] = 4, ['x'] = 22, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['race_guard_off'] = true, ['calls_for_help'] = true, ['allow_random'] = false },
+	{ ['t'] = 160, ['d'] = 1, ['side'] = 4, ['x'] = 24, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['buff'] = 'A41:B9:C19', ['race_guard_off'] = true, ['allow_random'] = false },
+	{ ['t'] = 161, ['d'] = 1, ['side'] = 4, ['x'] = 24, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['buff'] = 'A20:B41:C9', ['race_guard_off'] = true, ['allow_random'] = false },
+	{ ['t'] = 161, ['d'] = 1, ['side'] = 4, ['x'] = 25, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['buff'] = 'A30:B41:C13', ['race_guard_off'] = true, ['allow_random'] = false },
+	{ ['t'] = 158, ['d'] = 1, ['side'] = 2, ['x'] = 25, ['lvl'] = 6, ['type'] = 'QQ_adjudicator', ['default'] = 'QQ_adjudicator', ['buff'] = 'A23:B28:C13', ['guard'] = true },
 
 	{ ['t'] = 160, ['d'] = 2, ['side'] = 4, ['x'] = 24, ['lvl'] = 3, ['type'] = 'AE_mrc_enchanters_Shadow_Blade', ['default'] = 'Spectre', ['beefy'] = 28, ['armored'] = 5 },
 	
 	{ ['t'] = 157, ['d'] = 3, ['side'] = 4, ['x'] = 24, ['lvl'] = 5, ['type'] = 'QQ_flying_nightmare', ['default'] = 'QQ_flying_nightmare', ['guard'] = true, ['bulky'] = 80 },
 
 	{ ['t'] = 157, ['d'] = 4, ['side'] = 4, ['x'] = 23, ['lvl'] = 2, ['type'] = 'AE_mag_Pain_Mistress', ['default'] = 'Wraith', ['beefy'] = 48, },
-	
+	{ ['t'] = 158, ['d'] = 1, ['side'] = 4, ['x'] = 21, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['allow_random'] = false },
+	{ ['t'] = 158, ['d'] = 1, ['side'] = 4, ['x'] = 22, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['allow_random'] = false },
+	{ ['t'] = 158, ['d'] = 1, ['side'] = 4, ['x'] = 23, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['allow_random'] = false },
+	{ ['t'] = 158, ['d'] = 1, ['side'] = 4, ['x'] = 24, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['allow_random'] = false },
+	{ ['t'] = 158, ['d'] = 1, ['side'] = 4, ['x'] = 25, ['lvl'] = 1, ['type'] = 'QQ_enslaved_soul', ['default'] = 'QQ_enslaved_soul', ['allow_random'] = false },
+
 	{ ['t'] = 156, ['d'] = 5, ['side'] = 4, ['x'] = 24, ['lvl'] = 4, ['type'] = 'AE_mag_Dark_Apostle', ['default'] = 'QQ_dark_witness', ['bulky'] = 40, },
 
-	{ ['t'] = 159, ['d'] = 6, ['side'] = 4, ['x'] = 23, ['lvl'] = 6, ['type'] = 'QQ_death', ['default'] = 'QQ_death', ['buff'] = 'A21:B41:C17', },
+	{ ['t'] = 159, ['d'] = 6, ['side'] = 4, ['x'] = 23, ['lvl'] = 6, ['type'] = 'QQ_death', ['default'] = 'QQ_death', ['buff'] = 'A21:B41:C17', ['race_guard_off'] = true, },
+
+	{ ['t'] = 159, ['d'] = 7, ['side'] = 4, ['x'] = 24, ['lvl'] = 6, ['type'] = 'QQ_blackness', ['default'] = 'QQ_blackness', ['buff'] = 'A44:B18:A46' },
 	
 	-- 160 / final boss
 	
