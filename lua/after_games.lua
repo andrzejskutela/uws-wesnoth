ad8558c48d3426b8b7bbcb56981595e9645e4b05
PenguinHerder <skutelaandrzej@yahoo.com>
2024-08-20 13:36:43 +0100

    fire weapons done

--- lua/after_games.lua
+++ lua/after_games.lua
@@ -38,7 +38,8 @@ local all_available_items = {
 	'magic_res','cold_res','phys_res','impact_res','fire_res','arcane_res','blade_res','pierce_res','hp_low','hp_med','hp_high',
 	'steadfast','regen','melee_dmg','ranged_dmg','ranged_acc','melee_parry','melee_poison','melee_slow','mp','feeding','leadership',
 	'drain','defense','skirm','first_strike','fear','discouragement','burns','golden_armor','heal','freezing_gem','field_disruption',
-	'armor_destruction','protection','double_attack','hitn_run','extra_strikes','rat_pack','icewind_aura','book','dragon_protection'
+	'armor_destruction','protection','double_attack','hitn_run','extra_strikes','rat_pack','icewind_aura','book','dragon_protection',
+	'fire_weapon'
 }
 
 local all_boosts_table = {
